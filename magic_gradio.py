cf881397b8be3f4b0eb675239b323121694b6aa5
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-09-06 14:54:06 +0900

    Update magic_gradio.py

--- magic_gradio.py
+++ magic_gradio.py
@@ -46,4 +46,4 @@ def generate_mesh(input_image):
 
 inputs = gr.inputs.Image(label="Image", type="pil")
 outputs = gr.Model3D(label="3D Mesh", clear_color=[1.0, 1.0, 1.0, 1.0])
-gr.Interface(generate_mesh, inputs, outputs).launch()
+gr.Interface(generate_mesh, inputs, outputs).launch(share=True)

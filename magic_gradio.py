40c6ff80a62c1d4a911d9fd6889f7363cd2aa8fa
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-09-06 16:46:35 +0900

    Update magic_gradio.py

--- magic_gradio.py
+++ magic_gradio.py
@@ -8,50 +8,50 @@ import shutil
 import time
 import tqdm
 
-#with gr.Blocks() as demo:
+with gr.Blocks() as demo:
     
-    #inputs = gr.inputs.Image(label="Image", type="pil")
-    #outputs = gr.Model3D(label="3D Mesh", clear_color=[1.0, 1.0, 1.0, 1.0])
-    #btn = gr.Button("Generate!")
+    inputs = gr.inputs.Image(label="Image", type="pil")
+    outputs = gr.Model3D(label="3D Mesh", clear_color=[1.0, 1.0, 1.0, 1.0])
+    btn = gr.Button("Generate!")
     
-def generate_mesh(input_image, progress=gr.Progress(track_tqdm=True)):
+    def generate_mesh(input_image, progress=gr.Progress(track_tqdm=True)):
 
-    input_path = "./input"
-    output_path = "./out"
-    image_path = input_path + "/input.png"
-    GPU_NUM = "1"
+        input_path = "./input"
+        output_path = "./out"
+        image_path = input_path + "/input.png"
+        GPU_NUM = "1"
 
-    if os.path.exists(input_path):
-        shutil.rmtree(input_path)
-    if os.path.exists(output_path):
-        shutil.rmtree(output_path)
+        if os.path.exists(input_path):
+            shutil.rmtree(input_path)
+        if os.path.exists(output_path):
+            shutil.rmtree(output_path)
 
-    os.mkdir(input_path)
-    input_image.save(image_path)
+        os.mkdir(input_path)
+        input_image.save(image_path)
 
-    cmd_1 = f"python preprocess_image.py --path {image_path}"
-    cmd_2 = f"bash scripts/magic123/run_both_priors.sh {GPU_NUM} nerf dmtet {input_path} 1 1"
+        cmd_1 = f"python preprocess_image.py --path {image_path}"
+        cmd_2 = f"bash scripts/magic123/run_both_priors.sh {GPU_NUM} nerf dmtet {input_path} 1 1"
 
 
-    try:
-        completed_process = subprocess.Popen(cmd_1.split(), stdout=subprocess.PIPE)
-        completed_process = subprocess.Popen(cmd_2.split(), stdout=subprocess.PIPE)
-        print(completed_process.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred: {e}")
-        print(e.stdout)
-        print(e.stderr)
+        try:
+            completed_process = subprocess.Popen(cmd_1.split(), stdout=subprocess.PIPE)
+            completed_process = subprocess.Popen(cmd_2.split(), stdout=subprocess.PIPE)
+            print(completed_process.stdout)
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+            print(e.stdout)
+            print(e.stderr)
 
-    output_name = f"./out/magic123-nerf-dmtet/magic123_input_nerf_dmtet/mesh/mesh.glb"
-    return output_name
+        output_name = f"./out/magic123-nerf-dmtet/magic123_input_nerf_dmtet/mesh/mesh.glb"
+        return output_name
 
-    #btn.click(generate_mesh, inputs, outputs)
+    btn.click(generate_mesh, inputs, outputs)
 
-image = Image.open("./0.png")
-generate_mesh(image)
+#image = Image.open("./0.png")
+#generate_mesh(image)
 
 #inputs = gr.inputs.Image(label="Image", type="pil")
 #outputs = gr.Model3D(label="3D Mesh", clear_color=[1.0, 1.0, 1.0, 1.0])
 #gr.Interface(generate_mesh, inputs, outputs).launch(share=True)
 
-#demo.queue(concurrency_count=20).launch(share=True)
+demo.queue(concurrency_count=20).launch(share=True)

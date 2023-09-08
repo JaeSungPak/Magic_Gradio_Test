e2f6ce611a1559df66ea0ab14acacbd6ed88bb8d
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-09-08 18:34:28 +0900

    Update utils.py

--- nerf/utils.py
+++ nerf/utils.py
@@ -1046,7 +1046,7 @@ class Trainer(object):
         stage_num = "Coarse Stage[1/2] / "
         if self.opt.dmtet:
             stage_num = "Fine Stage[2/2] / "
-        for epoch in tqdm.tqdm(range(self.epoch + 1, max_epochs + 1), desc=stage_num+"epoch"):
+        for epoch in tqdm.tqdm(range(1, max_epochs + 1), desc=stage_num+"epoch"):
             self.epoch = epoch
 
             self.train_one_epoch(train_loader, max_epochs)
